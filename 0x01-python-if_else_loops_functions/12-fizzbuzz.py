311a8e69f98ed2ad1e6cc8ebc7fca7eb775a1fb0
Emyjakarta <ukonud4@gmail.com>
2023-11-28 17:44:04 +0100

    FIZZBUZZ

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for q in range(1, 101):
+        if q % 3 == 0 and q % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif q % 3 == 0:
+            print("Fizz", end=" ")
+        elif q % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{} ".format(q), end="")
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,10 +1,4 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-def islower(c):
-def uppercase(s):
-def print_last_digit(number):
-def add(a, b):
-def pow(a, b):
-
 #endif /*LISTS_H*/
