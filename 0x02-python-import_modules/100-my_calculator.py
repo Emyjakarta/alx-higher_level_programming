c82e9a97f716b8e133da08cae752a97801086fa4
Emyjakarta <ukonud4@gmail.com>
2023-11-30 14:16:00 +0100

    correcting calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """program that imports all functions from the file calculator_1.py and handles basic operations"""
-from calculator_1 import add, sub, mul, div
-import sys
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
 
-if len(sys.argv) - 1 != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-operator = {"+": add, "-": sub, "*": mul, "/": div}
-if sys.argv[2] not in list(operator.keys()):
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    operator = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
