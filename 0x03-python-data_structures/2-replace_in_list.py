d3f7d3a0dbc1401d0381d89254f9818f182601a6
Emyjakarta <ukonud4@gmail.com>
2023-12-02 13:32:06 +0100

    Completing the else conditional block for task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,7 @@ def replace_in_list(my_list, idx, element):
     length = len(my_list)
     if idx < 0 or idx >= length:
         return my_list
-    my_list[idx] = element
-    return my_list
+    else:
+        my_list[idx] = element
+        return my_list
 
