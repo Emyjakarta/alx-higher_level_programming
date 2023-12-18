23ad25b952fe5ce0bbdbca53685beba94fbd6f40
Emyjakarta <ukonud4@gmail.com>
2023-12-18 14:47:10 +0100

    test

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -6,7 +6,8 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             result += (a ** b) / i
-        except ValueError:
+        except:
+            pass
             result = b + a
             break
     return result
