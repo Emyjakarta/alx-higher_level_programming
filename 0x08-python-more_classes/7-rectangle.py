8f4283d24684e4bf17f11105e181393746a39554
Emyjakarta <ukonud4@gmail.com>
2024-01-04 17:38:27 +0100

    Cleared

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -56,7 +56,8 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
+        return '\n'.join([str(self.print_symbol) * self.__width
+                         for _ in range(self.__height)])
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.__width}, {self.__height})'
