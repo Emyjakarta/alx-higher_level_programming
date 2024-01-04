69eee82fb01722c58239a58e6a4a4535e88ad8ce
Emyjakarta <ukonud4@gmail.com>
2024-01-04 14:01:06 +0100

    class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+
+"class Rectangle that defines a rectangle by: (based on 0-rectangle.py)"
+
+
 class Rectangle:
+    "class Rectangle that defines a rectangle by: (based on 0-rectangle.py)"
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
