29ad093027302f385a57934ecf229a70b05b715d
Emyjakarta <ukonud4@gmail.com>
2024-01-04 17:09:19 +0100

    removed errors

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -14,6 +14,7 @@ class Rectangle:
     exception with the message width must be >= 0
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
