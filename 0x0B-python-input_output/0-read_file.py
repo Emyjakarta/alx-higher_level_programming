546f92f72abd00af859b4f33466f49faec1bc6df
Emyjakarta <ukonud4@gmail.com>
2024-01-09 12:03:52 +0100

    function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that reads a text file (UTF8)
+and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """You must use the with statement
+    You don’t need to manage file permission
+    or file doesn't exist exceptions.
+    """
+    try:
+        with open(filename, encoding='utf-8') as file:
+            print(file.read(), end='')
+    except FileNotFoundError:
+        pass
