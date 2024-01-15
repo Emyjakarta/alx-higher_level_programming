d7bedc2bf51945d2ef0189f1c0e1694b6bbd0443
Emyjakarta <ukonud4@gmail.com>
2024-01-15 18:16:05 +0100

    fixed documentation errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,9 +22,6 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         "Set the width of the rectangle"
-        #self.__validate_int_value("width", value)
-        #self.__validate_positive_value("width", value)
-        #if not isinstance(value, int):
         if (type(value) is not int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -39,10 +36,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         "Set the height of the rectangle"
-        #self.__validate_int_value("height", value)
-        #self.__validate_positive_value("height", value)
         if (type(value) is not int):
-        #if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -56,10 +50,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         "Set the x-coordinate of the rectangle."
-        #self.__validate_int_value("x", value)
-        #self.__validate_non_negative_value("x", value)
         if (type(value) is not int):
-        #if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -73,10 +64,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         "Set the y-coordinate of the rectangle."
-        #self.__validate_int_value("y", value)
-        #self.__validate_non_negative_value("y", value)
         if (type(value) is not int):
-        #if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
@@ -94,8 +82,6 @@ class Rectangle(Base):
             print()
         for _ in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
-        #for _ in range(self.__height):
-         #   print("#" * self.__width)
 
     def update(self, *args, **kwargs):
         """update attributes with no_keyword arguments
@@ -104,8 +90,6 @@ class Rectangle(Base):
         attr_list = ["id", "width", "height", "x", "y"]
         if args:
             for i in range(min(len(args), len(attr_list))):
-                #print("attr_list:", attr_list)
-                #print("args:", args)
                 setattr(self, attr_list[i], args[i])
         elif kwargs:
             for key, value in kwargs.items():
@@ -125,28 +109,3 @@ class Rectangle(Base):
                 "x": self.__x,
                 "y": self.__y
                 }
-
-#     def __validate_int_value(self, attr_name, value):
-#         "Validate if the value is an integer."
-#         if not isinstance(value, int):
-#             raise TypeError(f"{attr_name} must be an integer")
-
-#     def __validate_positive_value(self, attr_name, value):
-#         "Validate if the value is a positive integer."
-#         if value <= 0:
-#             raise ValueError(f"{attr_name} must be > 0")
-
-#     def __validate_non_negative_value(self, attr_name, value):
-#         "Validate if the value is a non-negative integer."
-#         if value < 0:
-#             raise ValueError(f"{attr_name} cannot be negative")
-
-# if __name__ == "__main__":
-#     r1 = Rectangle(10, 2)
-#     print(r1.id)
-
-#     r2 = Rectangle(2, 10)
-#     print(r2.id)
-
-#     r3 = Rectangle(10, 2, 0, 0, 12)
-#     print(r3.id)
