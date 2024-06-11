37f09664514781978f02be3a025d74bcafcfed9a
Emyjakarta <ukonud4@gmail.com>
2024-06-11 15:01:57 +0100

    JavaScript script that fetches and prints how to say “Hello” depending on the language

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,13 @@
+// JavaScript that fetches and prints
+// how to say “Hello” depending on the language
+
+const url = 'https://hellosalut.stefanbohacek.dev/?lang=';
+
+$(document).ready(() => {
+  $('input#btn_translate').on('click', () => {
+    const lang = $('input#language_code').val();
+    $.get(url + lang, (data) => {
+      $('div#hello').text(data.hello);
+    });
+  });
+});
