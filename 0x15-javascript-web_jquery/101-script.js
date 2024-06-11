136e457fdaa1427c15d8aacef2e7481fdab07d2f
Emyjakarta <ukonud4@gmail.com>
2024-06-11 15:00:50 +0100

    JavaScript script that adds, removes and clears LI elements from a list when the user clicks

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,15 @@
+// JavaScript that adds, removes and clears `<li>
+// elements from a list when the user clicks
+$(document).ready(() => {
+  $('div#add_item').on('click', () => {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+
+  $('div#remove_item').on('click', () => {
+    $('ul.my_list li').last().remove();
+  });
+
+  $('div#clear_list').on('click', () => {
+    $('ul.my_list').empty();
+  });
+});
